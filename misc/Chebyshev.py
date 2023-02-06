a1233a37554659f9cc991d6939298d7b88a3da8a
VLSF <fanaskov.vladimir@gmail.com>
2023-02-06 18:58:21 +0300

    move channel axis to 0, remove constant from indefinite integral

--- misc/Chebyshev.py
+++ misc/Chebyshev.py
@@ -10,44 +10,44 @@ def Chebyshev_grid(n):
 
 @jit
 def values_to_coefficients(values):
-    # input array has shape `(n1, n2, ..., c)`, where c stands for channels
-    # transform values of the function on the Chebyshev grid to coefficients of Chebyshev series
-    D = values.shape
-    transposition = [i for i in range(1, len(D)-1)] + [0, len(D)-1]
-    for N in D[:-1]:
-        values = jnp.real(jnp.fft.rfft(jnp.pad(values, [(0, N-2)] + [(0, 0)]*(len(D)-1), mode="reflect"), axis=0))/(N-1)
-        values = values.at[0].set(values[0]/2).at[-1].set(values[-1]/2)
-        values = values*((-1)**jnp.arange(N, dtype=int)).reshape([-1]+[1]*(len(D)-1))
-        values = jnp.transpose(values, transposition)
+    '''
+    Input array has shape `(c, n1, n2, ...)`, where c stands for channels.
+    Transform values of the function on the Chebyshev grid to coefficients of Chebyshev series.
+    '''
+    flip = (-1)**jnp.arange(max(values.shape[1:]), dtype=int)
+    for N in values.shape[1:][::-1]:
+        values = jnp.real(jnp.fft.rfft(jnp.pad(values, [(0, 0)]*(values.ndim-1) + [(0, N-2)], mode="reflect"), axis=-1))
+        values = values.at[..., 0].set(values[..., 0]/2).at[..., -1].set(values[..., -1]/2) / (N-1)
+        values = values*flip[:N].reshape([1]*(values.ndim-1) + [-1])
+        values = jnp.moveaxis(values, -1, 0)
+    values = jnp.moveaxis(values, -1, 0)
     return values
 
 @jit
 def coefficients_to_values(coefficients):
-    # x = coefficients_to_values(values_to_coefficients(x)) up to round-off error
-    D = coefficients.shape
-    transposition = [i for i in range(1, len(D)-1)] + [0, len(D)-1]
-    for N in D[:-1]:
-        coefficients = coefficients*((-1)**jnp.arange(N, dtype=int)).reshape([-1]+[1]*(len(D)-1))
-        coefficients = jnp.pad((N-1)*coefficients.at[0].set(coefficients[0]*2).at[-1].set(coefficients[-1]*2), [(0, N-2)] + [(0, 0)]*(len(D)-1), mode="reflect")
-        coefficients = jnp.real(jnp.fft.rfft(coefficients, axis=0))/(N-1)/2
-        coefficients = jnp.transpose(coefficients, transposition)
+    '''
+    x = coefficients_to_values(values_to_coefficients(x)) up to round-off error
+    '''
+    flip = (-1)**jnp.arange(max(coefficients.shape[1:]), dtype=int)
+    for N in coefficients.shape[1:][::-1]:
+        coefficients = coefficients*flip[:N].reshape([1]*(coefficients.ndim-1) + [-1]) / 2
+        coefficients = coefficients.at[..., 0].set(coefficients[..., 0]*2).at[..., -1].set(coefficients[..., -1]*2)
+        coefficients = jnp.real(jnp.fft.rfft(jnp.pad(coefficients, [(0, 0)]*(coefficients.ndim-1) + [(0, N-2)], mode="reflect"), axis=-1))
+        coefficients = jnp.moveaxis(coefficients, -1, 0)
+    coefficients = jnp.moveaxis(coefficients, -1, 0)
     return coefficients
 
 @partial(jit, static_argnums=1)
 def integrate(coeff, axis):
-    # find indefinite integral along `axis`
+    '''
+    find indefinite integral along `axis`
+    '''
     n = coeff.shape[axis]
     sh = [-1 if i == axis else 1 for i in range(len(coeff.shape))]
     w_0 = jnp.hstack([jnp.array([1, 1/4]), 1 / jnp.arange(3, n+2) / 2]).reshape(sh)
     w_1 = jnp.hstack([jnp.array([0, 1/4]), - 1 /  jnp.arange(1, n) / 2]).reshape(sh)
     coeff = jnp.pad(coeff, [(0, 1) if i == axis else (0, 0) for i in range(len(coeff.shape))])
     coeff = jnp.roll(w_0*coeff, 1, axis=axis) + jnp.roll(w_1*coeff, -1, axis=axis)
-    # correct to have indefinite integral from -1 to x
-    coeff = coeff.at[tuple([0]*(len(coeff.shape) - 1))].set(0)
-    w = (-1)**jnp.arange(n+1, dtype=int)
-    coeff = jnp.moveaxis(coeff, axis, 0)
-    coeff = coeff.at[0].set(-dot_general(w + 0., coeff, (((0,), (0,)), ((), ()))))
-    coeff = jnp.moveaxis(coeff, 0, axis)
     return coeff
 
 def get_interpolation_matrix(evaluate_at, m):
